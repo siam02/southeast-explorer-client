c030c9f3bd94c57a27fc0ecf787aeb7491d43d75
siam02 <siam.jaman02@gmail.com>
2024-04-30 19:34:23 +0600

    Navbar Responsive issue fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,14 +33,14 @@ const Navbar = () => {
             <div className="navbar max-w-7xl mx-auto">
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <div tabIndex={0} role="button" className="btn p-0 btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 text-black dark:text-gray-300 rounded-box w-52">
                             {navLinks}
                         </ul>
                     </div>
-                    <Link to='/' className="btn btn-ghost text-white font-bold text-lg">Southeast Explorer</Link>
+                    <Link to='/' className="btn btn-ghost p-2 text-white font-bold text-lg">Southeast Explorer</Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu gap-10 text-white menu-horizontal px-1">
